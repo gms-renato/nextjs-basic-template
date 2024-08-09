29df48ab4104adc70c0a4ce448a27dc74ad061a4
Renato Budiman <renato.budiman@gms.church>
2024-08-09 13:38:52 +0700

    default font color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import daisyui from 'daisyui'
 
 const config: Config = {
   content: [
@@ -6,15 +7,27 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
+  plugins: [daisyui],
+  daisyui: {
+    themes: [{
+      light: {
+        ...require("daisyui/src/theming/themes")["light"],
+        "color-scheme": "light",
+        primary: "#10408B",
+        secondary: "#463AA2",
+        accent: "#C148AC",
+        neutral: "#021431",
+        "base-100": "#ffffff",
+        "base-200": "#F2F7FF",
+        "base-300": "#E3E9F4",
+        "base-content": "#394E6A",
+        info: "#93E7FB",
+        success: "#0D8C51",
+        warning: "#F0BF14",
+        error: "#C61414",
+
+      }
+    }]
+  }
 };
 export default config;
