13fb0acbde33ac90c4f1798118b2779a48175e10
Renato Budiman <renato.budiman@gms.church>
2024-08-10 13:43:33 +0700

    specific daisyui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,5 @@
 import type { Config } from "tailwindcss";
-import daisyui from 'daisyui'
+import daisyui from '@renato92/daisyui'
 
 const config: Config = {
   content: [
@@ -7,27 +7,6 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  plugins: [daisyui],
-  daisyui: {
-    themes: [{
-      light: {
-        ...require("daisyui/src/theming/themes")["light"],
-        "color-scheme": "light",
-        primary: "#10408B",
-        secondary: "#463AA2",
-        accent: "#C148AC",
-        neutral: "#021431",
-        "base-100": "#ffffff",
-        "base-200": "#F2F7FF",
-        "base-300": "#E3E9F4",
-        "base-content": "#394E6A",
-        info: "#93E7FB",
-        success: "#0D8C51",
-        warning: "#F0BF14",
-        error: "#C61414",
-
-      }
-    }]
-  }
+  plugins: [daisyui]
 };
 export default config;
